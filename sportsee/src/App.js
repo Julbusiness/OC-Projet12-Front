e5274543b89c2f0ab3dc5080762f8310a7269bb0
Julbusiness <julien.dutruch@yahoo.fr>
2023-03-22 12:27:54 +0100

    mise en place de la navbar, des routes et de la page 404

--- sportsee/src/App.js
+++ sportsee/src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
+// import elements react et react-router-dom
+import { Routes, Route } from "react-router-dom";
+
+// import des composants
+import Navbar from "./Components/Navbar/Navbar";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Home from "./Pages/Home/Home";
+import Profil from "./Pages/Profil/Profil";
+import Setting from "./Pages/Setting/Setting";
+import Community from "./Pages/Community/Community";
+import NotFound from "./Pages/NotFound/NotFound";
+
+// import CSS
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-        <h1>Welcome</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar />
+			<Sidebar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/profil" element={<Profil />} />
+				<Route path="/setting" element={<Setting />} />
+				<Route path="/community" element={<Community />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ sportsee/src/Components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+import logo from "../../Assets/logo.png";
+
+export default function Navbar() {
+	return (
+		<header className="header">
+			<nav className="nav-container">
+				<div className="group">
+					<img src={logo} alt="logo" className="logo-header" />
+					<p className="logo-name">SportSee</p>
+				</div>
+				<ul className="liste-nav">
+					<NavLink to="/" className="items">
+						Accueil
+					</NavLink>
+					<NavLink to="/profil" className="items">
+						Profil
+					</NavLink>
+					<NavLink to="/setting" className="items">
+						Réglage
+					</NavLink>
+					<NavLink to="/community" className="items">
+						Communauté
+					</NavLink>
+				</ul>
+			</nav>
+		</header>
+	);
+}
