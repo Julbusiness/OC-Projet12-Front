deaf871203efc16dc0fbbf0c8df4f768951a09c7
Julbusiness <julien.dutruch@yahoo.fr>
2023-03-22 14:42:30 +0100

    mise en place de la sidebar et du copyright

--- sportsee/src/Components/Sidebar/Sidebar.js
+++ sportsee/src/Components/Sidebar/Sidebar.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./Sidebar.css";
+import logo1 from "../../Assets/sidebar1.png";
+import logo2 from "../../Assets/sidebar2.png";
+import logo3 from "../../Assets/sidebar3.png";
+import logo4 from "../../Assets/sidebar4.png";
 
 export default function Sidebar() {
-  return (
-    <div>Sidebar</div>
-  )
+	return (
+		<div className="sidebar">
+			<div className="logo-container">
+				<div className="bg-logo">
+					<img src={logo1} alt="" className="logo-side" />
+				</div>
+				<div className="bg-logo">
+					<img src={logo2} alt="" className="logo-side" />
+				</div>
+				<div className="bg-logo">
+					<img src={logo3} alt="" className="logo-side" />
+				</div>
+				<div className="bg-logo">
+					<img src={logo4} alt="" className="logo-side" />
+				</div>
+			</div>
+			<div className="copyright">&copy; Copyright SportSee 2020</div>
+		</div>
+	);
 }
--- sportsee/src/Pages/Profil/Profil.js
+++ sportsee/src/Pages/Profil/Profil.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import "./Profil.css"
 
 export default function Profil() {
   return (
-    <div>Profil</div>
+    <div className="profil">Profil</div>
   )
 }
