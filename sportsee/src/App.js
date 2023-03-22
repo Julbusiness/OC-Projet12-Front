1e2058030b0326fa16683b8802f642fba7e451e0
Julbusiness <julien.dutruch@yahoo.fr>
2023-03-22 19:14:32 +0100

    mise en place de quelques element dynamiques saud graphiques

--- sportsee/src/App.js
+++ sportsee/src/App.js
@@ -1,9 +1,6 @@
 // import elements react et react-router-dom
 import { Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
-
 // import des composants
-import { getApi } from "./Components/Api/Api"
 import Navbar from "./Components/Navbar/Navbar";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Home from "./Pages/Home/Home";
@@ -11,30 +8,17 @@ import Profil from "./Pages/Profil/Profil";
 import Setting from "./Pages/Setting/Setting";
 import Community from "./Pages/Community/Community";
 import NotFound from "./Pages/NotFound/NotFound";
-
 // import CSS
 import "./App.css";
 
-// console.log(Api)
-
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    async function getApiLoad() {
-      const data = await getApi()
-      setData(data)
-    }
-    getApiLoad()
-  }, []);
-
 	return (
 		<div className="App">
 			<Navbar />
 			<Sidebar />
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="/user/:id" element={<Profil data={data}/>} />
+				<Route path="/user/:id" element={<Profil />} />
 				<Route path="/setting" element={<Setting />} />
 				<Route path="/community" element={<Community />} />
 				<Route path="*" element={<NotFound />} />
--- sportsee/src/Pages/Profil/Profil.js
+++ sportsee/src/Pages/Profil/Profil.js
@@ -1,13 +1,31 @@
+import { useState, useEffect } from "react";
+import { getApi } from "../../Components/Api/Api";
 import "./Profil.css";
+import calories from "../../Assets/calories-icon.png";
+import proteines from "../../Assets/protein-icon.png";
+import glucides from "../../Assets/carbs-icon.png";
+import lipides from "../../Assets/fat-icon.png";
 
-export default function Profil(data) {
+export default function Profil() {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		async function getApiLoad() {
+			const data = await getApi();
+			setData(data);
+		}
+		getApiLoad();
+	}, []);
+
+	const element = data.data;
 
 	return (
+		element && (
 			<div className="container">
 				<div className="user-info">
 					<h1>
 						<span className="bonjour">Bonjour</span>
-						<span className="user-name"> {data.data.data.userInfos.firstName}</span>
+						<span className="user-name"> {element.userInfos.firstName}</span>
 					</h1>
 				</div>
 				<p className="profil-text">
@@ -15,14 +33,39 @@ export default function Profil(data) {
 				</p>
 				<div className="grid-container">
 					<div className="div1">First</div>
-					<div className="div2">Second</div>
-					<div className="div3">third</div>
-					<div className="div4">fourth</div>
-					<div className="div5">fifth</div>
+					<div className="calories">
+						<img src={calories} alt="" className="icon" />
+						<div className="container-info">
+							<p className="calDyn">{element.keyData.calorieCount}kCal</p>
+							<p className="calFix">Calories</p>
+						</div>
+					</div>
+					<div className="proteines">
+						<img src={proteines} alt="" className="icon" />
+						<div className="container-info">
+							<p className="calDyn">{element.keyData.proteinCount}g</p>
+							<p className="calFix">Proteines</p>
+						</div>
+					</div>
+					<div className="glucides">
+						<img src={glucides} alt="" className="icon" />
+						<div className="container-info">
+							<p className="calDyn">{element.keyData.carbohydrateCount}g</p>
+							<p className="calFix">Glucides</p>
+						</div>
+					</div>
+					<div className="lipides">
+						<img src={lipides} alt="" className="icon" />
+						<div className="container-info">
+							<p className="calDyn">{element.keyData.lipidCount}g</p>
+							<p className="calFix">Lipides</p>
+						</div>
+					</div>
 					<div className="div6">sixth</div>
 					<div className="div7">seventh</div>
 					<div className="div8">height</div>
 				</div>
 			</div>
+		)
 	);
 }
